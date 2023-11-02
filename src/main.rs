1b138ab82c465cc336140b3e44b4008ff21fa6a0
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-11-02 15:38:47 +0900

    feat:and

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod algorithm;
 
 fn main(){
-    algorithm::bottom_0::main();
-    // algorithm::bottom_1::main();
+    // algorithm::bottom_0::main();
+    algorithm::bottom_1::main();
     // algorithm::bottom_2::main();
 
 }
