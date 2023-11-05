3de2a2a5b8df710ece1b24668502f908ef04dc04
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-11-05 17:53:31 +0900

    feat:test

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,2 +1,3 @@
 pub mod bottom_0;
 pub mod bottom_1;
+pub mod bottom_2;
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod algorithm;
 
 fn main(){
     // algorithm::bottom_0::main();
-    algorithm::bottom_1::main();
-    // algorithm::bottom_2::main();
+    // algorithm::bottom_1::main();
+    algorithm::bottom_2::main();
 
 }
