1368e4d092e413e735bef784c06eda741b73cc55
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-12-11 11:08:07 +0900

    feat:step function

--- src/activation_function/step.rs
+++ src/activation_function/step.rs
@@ -1,3 +1,17 @@
+use ndarray::prelude::*;
+/*Step function */
+fn step (x:f64)->f64 {
+    if x>0.0{
+        return 1.0
+    }else{
+        return 0.0
+    }
+}
+
+fn step_function(x: &Array1<f64>) -> Array1<f64> {
+    x.map(|&val| if val > 0.0{ 1.0 } else { 0.0 })
+}
+
 pub fn main(){
     
 }
