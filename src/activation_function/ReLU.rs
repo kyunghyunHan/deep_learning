4ff6048de9d4c625d23579b9a16524a874351098
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-12-19 18:22:31 +0900

    feat:ReLU

--- src/activation_function/ReLU.rs
+++ src/activation_function/ReLU.rs
@@ -1,3 +1,19 @@
+
+use ndarray::prelude::*;
+
+
 pub fn main(){
     
 }
+
+fn relu_function(x: f64) -> f64 {
+    if x > 0.0 {
+        x
+    } else {
+        0.0
+    }
+}
+
+fn relu(x: &Array1<f64>) -> Array1<f64> {
+    x.mapv(|element| if element > 0.0 { element } else { 0.0 })
+  }
