8c35b48e270df002126a24ac24757eeeb3781911
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-12-24 22:38:38 +0900

    feat:test

--- src/math/numpy.rs
+++ src/math/numpy.rs
@@ -8,9 +8,7 @@ pub fn main(){
 
     println!("{}",a.len());
     println!("{:?}",a.shape());
-    println!("{:?}",a.t);
-
-
+   
     let b= arr2(&[[1,2,3,4],[5,6,7,8]]);
     println!("{}",b);
     println!("{:?}",b.shape())
