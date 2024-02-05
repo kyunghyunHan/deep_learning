99a715c894cb6f8637bb5f092011cb6703dd8c8e
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-02-05 16:50:48 +0900

    feat:bottom_03

--- src/algorithm/bottom_3.rs
+++ src/algorithm/bottom_3.rs
@@ -1,5 +1,34 @@
-use ndarray::prelude::*;
+use std::ops::Index;
 
+use super::weight;
+use ndarray::prelude::*;
+use polars::prelude::*;
+
+struct Mnist {
+    x_train: Array2<i32>,
+    y_train: Array1<i32>,
+}
+impl Mnist {
+    fn load_mnist() -> Mnist {
+        let x_train = CsvReader::from_path("./dataset/mnist/x_train.csv")
+            .unwrap()
+            .has_header(false)
+            .finish()
+            .unwrap()
+            .to_ndarray::<Int32Type>(IndexOrder::Fortran)
+            .unwrap();
+        let y_train = CsvReader::from_path("./dataset/mnist/y_train.csv")
+            .unwrap()
+            .has_header(false)
+            .finish()
+            .unwrap()
+            .to_ndarray::<Int32Type>(IndexOrder::Fortran)
+            .unwrap()
+            .index_axis(Axis(1), 0)
+            .to_owned();
+        Mnist { x_train, y_train }
+    }
+}
 /*신경망 학습 */
 pub fn main() {
     // 오차제곱합
@@ -8,6 +37,10 @@ pub fn main() {
 
     println!("{}", sum_squares_error(&y, &t));
     println!("{}", cross_entropy_error(&y, &t));
+    let x_train = Mnist::load_mnist().x_train;
+    let y_train = Mnist::load_mnist().y_train;
+    println!("{:?}", x_train.shape());
+    println!("{:?}", y_train.shape());
 }
 
 fn sum_squares_error(y: &Array1<f64>, t: &Array1<f64>) -> f64 {
@@ -19,9 +52,8 @@ fn sum_squares_error(y: &Array1<f64>, t: &Array1<f64>) -> f64 {
     0.5 * squared_diff
 }
 
-
-fn cross_entropy_error(y: &Array1<f64>, t: &Array1<f64>)->f64{
-    let delta= 1e-7;
+fn cross_entropy_error(y: &Array1<f64>, t: &Array1<f64>) -> f64 {
+    let delta = 1e-7;
     let y = y.map(|&y_i| y_i + delta);
 
     let log_y = y.iter().zip(t.iter()).map(|(&y_i, &t_i)| t_i * y_i.ln());
