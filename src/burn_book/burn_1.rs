f914cad948ec7b21254a4bb1dde76ad03bb35fd7
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-05-16 17:22:31 +0900

    feat:pytorch

--- src/burn_book/burn_1.rs
+++ src/burn_book/burn_1.rs
@@ -1,266 +1,266 @@
-use burn::{
-    config::Config,
-    module::Module,
-    nn::{
-        conv::{Conv2d, Conv2dConfig},//기본convolution
-        pool::{AdaptiveAvgPool2d, AdaptiveAvgPool2dConfig},
-        Dropout, DropoutConfig, Linear, LinearConfig, ReLU,
-        loss::CrossEntropyLoss
-    },
-    tensor::{
-        backend::{
-            Backend,AutodiffBackend
-        }, Data, ElementConversion, Int, Tensor
-    },
-    train::{
-        ClassificationOutput,TrainStep,TrainOutput,ValidStep,LearnerBuilder,
-        metric::{
-            AccuracyMetric,
-            LossMetric
-        }
-    },
-    optim::AdamConfig,
-    data::{
-        dataloader::{
-        DataLoaderBuilder,batcher::Batcher
-        },
-        dataset::source::huggingface::{
-            MNISTDataset,MNISTItem
-        }
-    },
-    record::CompactRecorder,
-    backend::{
-        Autodiff,
-        Wgpu,
-        wgpu::AutoGraphicsApi
-    }
-};
+// use burn::{
+//     config::Config,
+//     module::Module,
+//     nn::{
+//         conv::{Conv2d, Conv2dConfig},//기본convolution
+//         pool::{AdaptiveAvgPool2d, AdaptiveAvgPool2dConfig},
+//         Dropout, DropoutConfig, Linear, LinearConfig,
+//         loss::CrossEntropyLoss
+//     },
+//     tensor::{
+//         backend::{
+//             Backend,AutodiffBackend
+//         }, Data, ElementConversion, Int, Tensor
+//     },
+//     train::{
+//         ClassificationOutput,TrainStep,TrainOutput,ValidStep,LearnerBuilder,
+//         metric::{
+//             AccuracyMetric,
+//             LossMetric
+//         }
+//     },
+//     optim::AdamConfig,
+//     data::{
+//         dataloader::{
+//         DataLoaderBuilder,batcher::Batcher
+//         },
+//         dataset::source::huggingface::{
+//             MNISTDataset,MNISTItem
+//         }
+//     },
+//     record::CompactRecorder,
+//     backend::{
+//         Autodiff,
+//         Wgpu,
+//         wgpu::AutoGraphicsApi
+//     }
+// };
 
-/*
-기본 convolution neural network
-두개의 convolution layer 
-두개의 linear layer
-pooling
-ReLU
+// /*
+// 기본 convolution neural network
+// 두개의 convolution layer 
+// 두개의 linear layer
+// pooling
+// ReLU
 
-*/
-#[derive(Module, Debug)]//딥러닝 모듈생성
-pub struct Model<B: Backend> {//BackEnd:새모델이 모든 벡엔드에서 실행할수 있게함
-    conv1: Conv2d<B>,
-    conv2: Conv2d<B>,
-    pool: AdaptiveAvgPool2d,
-    dropout: Dropout,
-    linear1: Linear<B>,
-    linear2: Linear<B>,
-    activation: ReLU,
-}
-//전방향 패스
-impl<B: Backend> Model<B> {
+// */
+// #[derive(Module, Debug)]//딥러닝 모듈생성
+// pub struct Model<B: Backend> {//BackEnd:새모델이 모든 벡엔드에서 실행할수 있게함
+//     conv1: Conv2d<B>,
+//     conv2: Conv2d<B>,
+//     pool: AdaptiveAvgPool2d,
+//     dropout: Dropout,
+//     linear1: Linear<B>,
+//     linear2: Linear<B>,
+//     activation: ReLU,
+// }
+// //전방향 패스
+// impl<B: Backend> Model<B> {
   
-    pub fn forward(&self, images: Tensor<B, 3>) -> Tensor<B, 2> {
-        let [batch_size, height, width] = images.dims();
+//     pub fn forward(&self, images: Tensor<B, 3>) -> Tensor<B, 2> {
+//         let [batch_size, height, width] = images.dims();
 
-        // Create a channel at the second dimension.
-        let x = images.reshape([batch_size, 1, height, width]);
-
-
-        let x = self.conv1.forward(x); // [batch_size, 8, _, _]
-        let x = self.dropout.forward(x);
-        let x = self.conv2.forward(x); // [batch_size, 16, _, _]
-        let x = self.dropout.forward(x);
-        let x = self.activation.forward(x);
-
-        let x = self.pool.forward(x); // [batch_size, 16, 8, 8]
-        let x = x.reshape([batch_size, 16 * 8 * 8]);
-        let x = self.linear1.forward(x);
-        let x = self.dropout.forward(x);
-        let x = self.activation.forward(x);
-
-        self.linear2.forward(x) // [batch_size, num_classes]
-    }
-
-    pub fn forward_classification(
-        &self,
-        images: Tensor<B, 3>,
-        targets: Tensor<B, 1, Int>,
-    ) -> ClassificationOutput<B> {
-        let output = self.forward(images);
-        let loss = CrossEntropyLoss::new(None).forward(output.clone(), targets.clone());
-
-        ClassificationOutput::new(loss, output, targets)
-    }
-
-}
-
-impl<B: AutodiffBackend> TrainStep<MNISTBatch<B>, ClassificationOutput<B>> for Model<B> {
-    fn step(&self, batch: MNISTBatch<B>) -> TrainOutput<ClassificationOutput<B>> {
-        let item = self.forward_classification(batch.images, batch.targets);
-
-        TrainOutput::new(self, item.loss.backward(), item)
-    }
-}
-
-impl<B: Backend> ValidStep<MNISTBatch<B>, ClassificationOutput<B>> for Model<B> {
-    fn step(&self, batch: MNISTBatch<B>) -> ClassificationOutput<B> {
-        self.forward_classification(batch.images, batch.targets)
-    }
-}
-//network의 기본 구성 요소
-//구성을 직렬화하여 모델 hyperprameter를 쉽게 저장
-#[derive(Config, Debug)]
-pub struct ModelConfig {
-    num_classes: usize,
-    hidden_size: usize,
-    #[config(default = "0.5")]
-    dropout: f64,
-}
-
-impl ModelConfig {
-    /// Returns the initialized model.
-    pub fn init<B: Backend>(&self) -> Model<B> {
-        Model {
-            //커널 크기 3사용
-            //채널 1에서 8로 확장
-            conv1: Conv2dConfig::new([1, 8], [3, 3]).init(),
-            //8에서 16으로 확장
-            conv2: Conv2dConfig::new([8, 16], [3, 3]).init(),
-            //적응형 평균 폴링 모듈을 사용 이미지의 차원을 8x8으로 축소
-            pool: AdaptiveAvgPool2dConfig::new([8, 8]).init(),
-            activation: ReLU::new(),
-            linear1: LinearConfig::new(16 * 8 * 8, self.hidden_size).init(),
-            linear2: LinearConfig::new(self.hidden_size, self.num_classes).init(),
-            dropout: DropoutConfig::new(self.dropout).init(),
-        }
-    }
-    pub fn init_with<B: Backend>(&self, record: ModelRecord<B>) -> Model<B> {
-        Model {
-            conv1: Conv2dConfig::new([1, 8], [3, 3]).init_with(record.conv1),
-            conv2: Conv2dConfig::new([8, 16], [3, 3]).init_with(record.conv2),
-            pool: AdaptiveAvgPool2dConfig::new([8, 8]).init(),
-            activation: ReLU::new(),
-            linear1: LinearConfig::new(16 * 8 * 8, self.hidden_size).init_with(record.linear1),
-            linear2: LinearConfig::new(self.hidden_size, self.num_classes)
-                .init_with(record.linear2),
-            dropout: DropoutConfig::new(self.dropout).init(),
-        }
-    }
-}
-/*
-data
-Batcher 구조체를 정의 => 텐서가 모델에 전달되기 전에  전송되어야 하는장치
-
-*/
-
-pub struct MNISTBatcher<B: Backend> {
-    device: B::Device,
-}
-
-impl<B: Backend> MNISTBatcher<B> {
-    pub fn new(device: B::Device) -> Self {
-        Self { device }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct MNISTBatch<B: Backend> {
-    pub images: Tensor<B, 3>,
-    pub targets: Tensor<B, 1, Int>,
-}
-
-impl<B: Backend> Batcher<MNISTItem, MNISTBatch<B>> for MNISTBatcher<B> {
-    fn batch(&self, items: Vec<MNISTItem>) -> MNISTBatch<B> {
-        let images = items
-            .iter()
-            .map(|item| Data::<f32, 2>::from(item.image))
-            .map(|data| Tensor::<B, 2>::from_data(data.convert()))
-            .map(|tensor| tensor.reshape([1, 28, 28]))
-            // Normalize: make between [0,1] and make the mean=0 and std=1
-            // values mean=0.1307,std=0.3081 are from the PyTorch MNIST example
-            // https://github.com/pytorch/examples/blob/54f4572509891883a947411fd7239237dd2a39c3/mnist/main.py#L122
-            .map(|tensor| ((tensor / 255) - 0.1307) / 0.3081)
-            .collect();
-
-        let targets = items
-            .iter()
-            .map(|item| Tensor::<B, 1, Int>::from_data(Data::from([(item.label as i64).elem()])))
-            .collect();
-
-        let images = Tensor::cat(images, 0).to_device(&self.device);
-        let targets = Tensor::cat(targets, 0).to_device(&self.device);
-
-        MNISTBatch { images, targets }
-    }
-}
-#[derive(Config)]
-pub struct TrainingConfig {
-    pub model: ModelConfig,
-    pub optimizer: AdamConfig,
-    #[config(default = 10)]
-    pub num_epochs: usize,
-    #[config(default = 64)]
-    pub batch_size: usize,
-    #[config(default = 4)]
-    pub num_workers: usize,
-    #[config(default = 42)]
-    pub seed: u64,
-    #[config(default = 1.0e-4)]
-    pub learning_rate: f64,
-}
-
-pub fn train<B: AutodiffBackend>(artifact_dir: &str, config: TrainingConfig, device: B::Device) {
-    std::fs::create_dir_all(artifact_dir).ok();
-    config
-        .save(format!("{artifact_dir}/config.json"))
-        .expect("Config should be saved successfully");
-
-    B::seed(config.seed);
-
-    let batcher_train = MNISTBatcher::<B>::new(device.clone());
-    let batcher_valid = MNISTBatcher::<B::InnerBackend>::new(device.clone());
-
-    let dataloader_train = DataLoaderBuilder::new(batcher_train)
-        .batch_size(config.batch_size)
-        .shuffle(config.seed)
-        .num_workers(config.num_workers)
-        .build(MNISTDataset::train());
-
-    let dataloader_test = DataLoaderBuilder::new(batcher_valid)
-        .batch_size(config.batch_size)
-        .shuffle(config.seed)
-        .num_workers(config.num_workers)
-        .build(MNISTDataset::test());
-
-    let learner = LearnerBuilder::new(artifact_dir)
-        .metric_train_numeric(AccuracyMetric::new())
-        .metric_valid_numeric(AccuracyMetric::new())
-        .metric_train_numeric(LossMetric::new())
-        .metric_valid_numeric(LossMetric::new())
-        .with_file_checkpointer(CompactRecorder::new())
-        .devices(vec![device])
-        .num_epochs(config.num_epochs)
-        .build(
-            config.model.init::<B>(),
-            config.optimizer.init(),
-            config.learning_rate,
-        );
-    let model_trained = learner.fit(dataloader_train, dataloader_test);
-    model_trained
-        .save_file(format!("{artifact_dir}/model"), &CompactRecorder::new())
-        .expect("Trained model should be saved successfully");
-}
+//         // Create a channel at the second dimension.
+//         let x = images.reshape([batch_size, 1, height, width]);
 
 
-pub fn main(){
-    let config = ModelConfig::new(10, 1024);
-     println!("{}",config);
-     type MyBackend = Wgpu<AutoGraphicsApi, f32, i32>;
-     type MyAutodiffBackend = Autodiff<MyBackend>;
-     let device = burn::backend::wgpu::WgpuDevice::default();
-     train::<MyAutodiffBackend>(
-        "./train",
-        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
-        device,
-    );
+//         let x = self.conv1.forward(x); // [batch_size, 8, _, _]
+//         let x = self.dropout.forward(x);
+//         let x = self.conv2.forward(x); // [batch_size, 16, _, _]
+//         let x = self.dropout.forward(x);
+//         let x = self.activation.forward(x);
+
+//         let x = self.pool.forward(x); // [batch_size, 16, 8, 8]
+//         let x = x.reshape([batch_size, 16 * 8 * 8]);
+//         let x = self.linear1.forward(x);
+//         let x = self.dropout.forward(x);
+//         let x = self.activation.forward(x);
+
+//         self.linear2.forward(x) // [batch_size, num_classes]
+//     }
+
+//     pub fn forward_classification(
+//         &self,
+//         images: Tensor<B, 3>,
+//         targets: Tensor<B, 1, Int>,
+//     ) -> ClassificationOutput<B> {
+//         let output = self.forward(images);
+//         let loss = CrossEntropyLoss::new(None).forward(output.clone(), targets.clone());
+
+//         ClassificationOutput::new(loss, output, targets)
+//     }
+
+// }
+
+// impl<B: AutodiffBackend> TrainStep<MNISTBatch<B>, ClassificationOutput<B>> for Model<B> {
+//     fn step(&self, batch: MNISTBatch<B>) -> TrainOutput<ClassificationOutput<B>> {
+//         let item = self.forward_classification(batch.images, batch.targets);
+
+//         TrainOutput::new(self, item.loss.backward(), item)
+//     }
+// }
+
+// impl<B: Backend> ValidStep<MNISTBatch<B>, ClassificationOutput<B>> for Model<B> {
+//     fn step(&self, batch: MNISTBatch<B>) -> ClassificationOutput<B> {
+//         self.forward_classification(batch.images, batch.targets)
+//     }
+// }
+// //network의 기본 구성 요소
+// //구성을 직렬화하여 모델 hyperprameter를 쉽게 저장
+// #[derive(Config, Debug)]
+// pub struct ModelConfig {
+//     num_classes: usize,
+//     hidden_size: usize,
+//     #[config(default = "0.5")]
+//     dropout: f64,
+// }
+
+// impl ModelConfig {
+//     /// Returns the initialized model.
+//     pub fn init<B: Backend>(&self) -> Model<B> {
+//         Model {
+//             //커널 크기 3사용
+//             //채널 1에서 8로 확장
+//             conv1: Conv2dConfig::new([1, 8], [3, 3]).init(),
+//             //8에서 16으로 확장
+//             conv2: Conv2dConfig::new([8, 16], [3, 3]).init(),
+//             //적응형 평균 폴링 모듈을 사용 이미지의 차원을 8x8으로 축소
+//             pool: AdaptiveAvgPool2dConfig::new([8, 8]).init(),
+//             activation: ReLU::new(),
+//             linear1: LinearConfig::new(16 * 8 * 8, self.hidden_size).init(),
+//             linear2: LinearConfig::new(self.hidden_size, self.num_classes).init(),
+//             dropout: DropoutConfig::new(self.dropout).init(),
+//         }
+//     }
+//     pub fn init_with<B: Backend>(&self, record: ModelRecord<B>) -> Model<B> {
+//         Model {
+//             conv1: Conv2dConfig::new([1, 8], [3, 3]).init_with(record.conv1),
+//             conv2: Conv2dConfig::new([8, 16], [3, 3]).init_with(record.conv2),
+//             pool: AdaptiveAvgPool2dConfig::new([8, 8]).init(),
+//             activation: ReLU::new(),
+//             linear1: LinearConfig::new(16 * 8 * 8, self.hidden_size).init_with(record.linear1),
+//             linear2: LinearConfig::new(self.hidden_size, self.num_classes)
+//                 .init_with(record.linear2),
+//             dropout: DropoutConfig::new(self.dropout).init(),
+//         }
+//     }
+// }
+// /*
+// data
+// Batcher 구조체를 정의 => 텐서가 모델에 전달되기 전에  전송되어야 하는장치
+
+// */
+
+// pub struct MNISTBatcher<B: Backend> {
+//     device: B::Device,
+// }
+
+// impl<B: Backend> MNISTBatcher<B> {
+//     pub fn new(device: B::Device) -> Self {
+//         Self { device }
+//     }
+// }
+
+// #[derive(Clone, Debug)]
+// pub struct MNISTBatch<B: Backend> {
+//     pub images: Tensor<B, 3>,
+//     pub targets: Tensor<B, 1, Int>,
+// }
+
+// impl<B: Backend> Batcher<MNISTItem, MNISTBatch<B>> for MNISTBatcher<B> {
+//     fn batch(&self, items: Vec<MNISTItem>) -> MNISTBatch<B> {
+//         let images = items
+//             .iter()
+//             .map(|item| Data::<f32, 2>::from(item.image))
+//             .map(|data| Tensor::<B, 2>::from_data(data.convert()))
+//             .map(|tensor| tensor.reshape([1, 28, 28]))
+//             // Normalize: make between [0,1] and make the mean=0 and std=1
+//             // values mean=0.1307,std=0.3081 are from the PyTorch MNIST example
+//             // https://github.com/pytorch/examples/blob/54f4572509891883a947411fd7239237dd2a39c3/mnist/main.py#L122
+//             .map(|tensor| ((tensor / 255) - 0.1307) / 0.3081)
+//             .collect();
+
+//         let targets = items
+//             .iter()
+//             .map(|item| Tensor::<B, 1, Int>::from_data(Data::from([(item.label as i64).elem()])))
+//             .collect();
+
+//         let images = Tensor::cat(images, 0).to_device(&self.device);
+//         let targets = Tensor::cat(targets, 0).to_device(&self.device);
+
+//         MNISTBatch { images, targets }
+//     }
+// }
+// #[derive(Config)]
+// pub struct TrainingConfig {
+//     pub model: ModelConfig,
+//     pub optimizer: AdamConfig,
+//     #[config(default = 10)]
+//     pub num_epochs: usize,
+//     #[config(default = 64)]
+//     pub batch_size: usize,
+//     #[config(default = 4)]
+//     pub num_workers: usize,
+//     #[config(default = 42)]
+//     pub seed: u64,
+//     #[config(default = 1.0e-4)]
+//     pub learning_rate: f64,
+// }
+
+// pub fn train<B: AutodiffBackend>(artifact_dir: &str, config: TrainingConfig, device: B::Device) {
+//     std::fs::create_dir_all(artifact_dir).ok();
+//     config
+//         .save(format!("{artifact_dir}/config.json"))
+//         .expect("Config should be saved successfully");
+
+//     B::seed(config.seed);
+
+//     let batcher_train = MNISTBatcher::<B>::new(device.clone());
+//     let batcher_valid = MNISTBatcher::<B::InnerBackend>::new(device.clone());
+
+//     let dataloader_train = DataLoaderBuilder::new(batcher_train)
+//         .batch_size(config.batch_size)
+//         .shuffle(config.seed)
+//         .num_workers(config.num_workers)
+//         .build(MNISTDataset::train());
+
+//     let dataloader_test = DataLoaderBuilder::new(batcher_valid)
+//         .batch_size(config.batch_size)
+//         .shuffle(config.seed)
+//         .num_workers(config.num_workers)
+//         .build(MNISTDataset::test());
+
+//     let learner = LearnerBuilder::new(artifact_dir)
+//         .metric_train_numeric(AccuracyMetric::new())
+//         .metric_valid_numeric(AccuracyMetric::new())
+//         .metric_train_numeric(LossMetric::new())
+//         .metric_valid_numeric(LossMetric::new())
+//         .with_file_checkpointer(CompactRecorder::new())
+//         .devices(vec![device])
+//         .num_epochs(config.num_epochs)
+//         .build(
+//             config.model.init::<B>(),
+//             config.optimizer.init(),
+//             config.learning_rate,
+//         );
+//     let model_trained = learner.fit(dataloader_train, dataloader_test);
+//     model_trained
+//         .save_file(format!("{artifact_dir}/model"), &CompactRecorder::new())
+//         .expect("Trained model should be saved successfully");
+// }
+
+
+// pub fn main(){
+//     let config = ModelConfig::new(10, 1024);
+//      println!("{}",config);
+//      type MyBackend = Wgpu<AutoGraphicsApi, f32, i32>;
+//      type MyAutodiffBackend = Autodiff<MyBackend>;
+//      let device = burn::backend::wgpu::WgpuDevice::default();
+//      train::<MyAutodiffBackend>(
+//         "./train",
+//         TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+//         device,
+//     );
     
-}
+// }
 
--- src/burn_book/mod.rs
+++ src/burn_book/mod.rs
@@ -1,3 +1,4 @@
 pub mod burn_0;
 pub mod burn_1;
 pub mod custom;
+pub mod pytorch;
